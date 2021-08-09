3d6e0f6fbe0c60f6d4581465374bea89d7ea0a46
dudikpal <59420449+dudikpal@users.noreply.github.com>
2021-08-09 05:07:40 +0200

    Initial commit

---
+++ src/test/java/training/vizsgaremekrecipes/VizsgaremekRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package training.vizsgaremekrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizsgaremekRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
